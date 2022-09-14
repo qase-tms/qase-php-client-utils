2576692d7e72311f5eae4544267646d567c10d45
in4s <28087198+in4s@users.noreply.github.com>
2022-09-14 17:31:56 +0400

    logging debug option support fix 3 LoggerInterface prefix default value removed

--- src/LoggerInterface.php
+++ src/LoggerInterface.php
@@ -6,7 +6,7 @@ namespace Qase\PhpClientUtils;
 
 interface LoggerInterface
 {
-    public function write(string $message, string $prefix = '[Qase reporter]'): void;
+    public function write(string $message, string $prefix): void;
 
-    public function writeln(string $message, string $prefix = '[Qase reporter]'): void;
+    public function writeln(string $message, string $prefix): void;
 }
